ad7312208f88ca0c98a0814d85cdb88df26caea8
kfriden <kfriden@verizon.net>
2024-02-10 13:12:00 -0500

    submitting lesson 1 homework

---
+++ lesson2.py
@@ -0,0 +1,35 @@
+# name = 'Emily'
+# age = 26
+# job_title = 'QA Manager'
+
+# print(name + " is a" + ' ' + str(age) + ' years old ' + job_title + ' of our company')
+
+
+# name_1 = 'Nanka'
+# name_2 = 'Kit'
+
+# sentence = '{} and {}'.format(name_1, name_2)
+# print(sentence)
+
+
+# name1 = 'Emily'
+# age1 = 26
+# job_title1 = 'QA Manager'
+# sentence2 = '{0} is a {1} years old {2} of our company'.format(name1, age1, job_title1)
+# print(sentence2)
+
+
+# name2 = 'Emily'
+# age2 = 26
+# job_title2 = 'QA Manager'
+
+# sentence3 = f'{name2} is a {age2} years old {job_title2} of our company'
+# print(sentence3)
+
+
+# python_string = "Python programming is fun!"
+# print(python_string[7:19])
+# print(python_string[7:27])
+# print(python_string[0:27:3])
+
+
---
+++ python_numbers.py
@@ -0,0 +1,35 @@
+# print(1 + 1)
+# print(50 - 90)
+# print(28 * 3)
+# print(-1 * 5)
+# print(478 / 2)
+# print(2 ** 8)
+
+# print(332 + 75.6)
+# print(500 // 2 ** 3)
+# print(-5 + 2)
+# print(100 % 3)
+
+
+# print(4 + 100 / 10 * 5)
+# print(4 + (100 / (10 * 5)))
+# print((4 + 100 / 10) * 5)
+# print(((4 + 100) / 10) * 5)
+
+# a = 15
+# b = 19
+# h = 15
+# print((a + b) * h / 2)
+
+# CALCULATE EXPENSES
+
+rent = 1200
+utilities = 300
+groceries = 450
+entertainment = 200
+total = rent + utilities + groceries + entertainment
+print (total)
+rent_percentage = ((rent/total) * 100)
+print(round(rent_percentage))
+
+
